11b6837ed46efde854bbebdcba1c3bba87006174
Nimisha <Nimisha@LAPTOP-QSHT39JU>
2022-09-02 12:40:15 +0530

    First commit.

---
+++ My_maven/src/main/java/java/Java.java
@@ -0,0 +1,11 @@
+package java;
+
+
+public class Java{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("java program is executed");
+	}
+
+}
---
+++ My_maven/src/test/java/TestHello.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class TestHello {
+	
+	
+	//public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	//System.out.println("Hello world to test");
+		//}
+
+		//or without main method we can execute the code
+		
+		@Test //annotation of junit
+		public void testMethod() {
+			System.out.println("Am a test engineer");
+
+}
+}
